60ae26377e8f6f9a877e7849d1b09992ee9466b1
mhuertascompany <marc.huertas@obspm.fr>
2025-11-04 21:41:58 +0100

    test

--- astroinfo/denario_astropt.py
+++ astroinfo/denario_astropt.py
@@ -1,5 +1,5 @@
 from denario import Denario
-den = Denario()
+den = Denario(project_dir="/n03data/huertas/python/Denario/astroinfo/project")
 
 data_description = r"""
 We have a multimodal dataset combining DESI spectroscopy and EUCLID imaging. The data is stored in a HF dataset in this
@@ -7,19 +7,19 @@ local folder: /n03data/huertas/desi_euclid/msiudek___astro_pt_euclid_desi_datase
 here: /n03data/huertas/desi_euclid/msiudek___astro_pt_euclid_training_dataset.  
 """
 
-den.set_data_description(data_description = data_description)
+#den.set_data_description(data_description = data_description)
 
 
-den.set_idea(idea=r"""
-Identify rare or unusual astronomical objects by leveraging the multimodal dataset combining DESI 
-spectroscopy and Euclid imaging data. The idea is to test whether multimodal data leads to more interesting
-anomalies. The plan is to use the astroPT model (https://github.com/Smith42/astroPT and aleady cloned in /n03data/huertas/python/astroPT) to obtain mutlimodal embeddings
-and then perform anomaly detection on those embeddings. For the anomaly detection we will train a density estimator such
-as a normalzing flow on the the embedding space and identify objects with low likelihood. The process will be also done
-with only Euclid images and we will compare the anomalies. The datasest need to be downloaded to 
-""")
+#den.set_idea(idea=r"""
+#Identify rare or unusual astronomical objects by leveraging the multimodal dataset combining DESI 
+#spectroscopy and Euclid imaging data. The idea is to test whether multimodal data leads to more interesting
+#anomalies. The plan is to use the astroPT model (https://github.com/Smith42/astroPT and aleady cloned in /n03data/huertas/python/astroPT) to obtain mutlimodal embeddings
+#and then perform anomaly detection on those embeddings. For the anomaly detection we will train a density estimator such
+#as a normalzing flow on the the embedding space and identify objects with low likelihood. The process will be also done
+#with only Euclid images and we will compare the anomalies. The datasest need to be downloaded to 
+#""")
 
-den.get_method(mode='cmbagent')
+#den.get_method(mode='cmbagent')
 
-den.get_results()
+den.get_results(max_n_steps=3, engineer_model="gemini-2.5-pro")
 
