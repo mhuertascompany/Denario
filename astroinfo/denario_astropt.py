d433548b38d114f64e5c0ed2d38970b5a84a78cf
mhuertascompany <marc.huertas@obspm.fr>
2025-11-05 12:32:50 +0100

    denario test

--- astroinfo/denario_astropt.py
+++ astroinfo/denario_astropt.py
@@ -3,8 +3,8 @@ den = Denario(project_dir="/n03data/huertas/python/Denario/astroinfo/project")
 
 data_description = r"""
 We have a multimodal dataset combining DESI spectroscopy and EUCLID imaging. The data is stored in a HF dataset in this
-local folder: /n03data/huertas/desi_euclid/msiudek___astro_pt_euclid_desi_dataset. we also have a euclid only imaging dataset avaialble 
-here: /n03data/huertas/desi_euclid/msiudek___astro_pt_euclid_training_dataset.  
+local folder: /n03data/huertas/desi_euclid/msiudek___astro_pt_euclid_desi_dataset. 
+The keywords were are going to use are ['VIS_image', 'NISP_Y_image', 'NISP_J_image', 'NISP_H_image'] for images and ['spectrum'] for spectra.
 """
 
 den.set_data_description(data_description = data_description)
@@ -15,9 +15,10 @@ Identify rare or unusual astronomical objects by leveraging the multimodal datas
 spectroscopy and Euclid imaging data. The idea is to test whether multimodal data leads to more interesting
 anomalies. The plan is to use the astroPT model (cloned in /n03data/huertas/python/astroPT) to obtain mutlimodal embeddings
 and then perform anomaly detection on those embeddings. We will start with the first part of the project first: obtain multimodal embeddings with astroPT and store them safely. We will worry later about anomalies.
-IMPORTANT: We will have to train a mulitmodal model from scratch, so you will have to go to the astroPT repo, read the code and understand how to do it. There's a script called scripts/train_multimodal.py in the repo that you could use as an example.
+IMPORTANT: We will have to train a mulitmodal model from scratch, so you will have to go to the astroPT repo, read the code and understand how to do it. There's a script called scripts/train_multimodal.py in the repo. USE IT and ADAPT IT.
+Nomally, the python environemnt is all set up. Avoid installing new packages. Use the packages imported there - do not invent new pakages / imports which will fail.
 We also have a checkpoint stored here: /n03data/huertas/python/models/astroPT/euclid_Q1/ckpt.pt of a model trained on only Eulcid images, that we can perhaps use for 1. obtaining embeddings on images only for comparison later 
-and 2. starting point for the multimodal training. Nomally, the python environemnt is all set up.             
+and 2. starting point for the multimodal training.              
 """)
 
 #den.set_idea(idea=r"""
